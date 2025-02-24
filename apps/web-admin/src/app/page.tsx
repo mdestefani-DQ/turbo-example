73c0ca81eae946be2e14a6ffe5f79572b411530e
mdestefani-DQ <marco.destefani@donq.io>
2025-02-24 11:31:36 +0100

    remove admin path

--- apps/web-admin/src/app/page.tsx
+++ apps/web-admin/src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Web() {
 
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Admin</h1>
       <form onSubmit={onSubmit}>
         <label htmlFor="name">Name </label>
         <input
