5fc5435ecacbc630b7c919916fbf4d52ddca2106
mdestefani-DQ <marco.destefani@donq.io>
2025-02-24 11:56:07 +0100

    rm other options

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,10 +4,5 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
-  serverRuntimeConfig: {
-    PUBLIC_PATH: __dirname + '/public',
-  },
+  basePath: '/',
 };
